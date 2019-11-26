e90e1fb8f4c6a4ee5babd4f7d811972b2aecfff5
tinle12345 <lethaitin@outlook.com>
2019-11-26 14:21:14 +0700

    them file

---
+++ WindowsApplication2/Form1.Designer.vb
@@ -0,0 +1,193 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.oklb = New System.Windows.Forms.Label()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.ronglb = New System.Windows.Forms.Label()
+        Me.dailb = New System.Windows.Forms.Label()
+        Me.daylb = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.SerialPort1 = New System.IO.Ports.SerialPort(Me.components)
+        Me.same = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'oklb
+        '
+        Me.oklb.BackColor = System.Drawing.Color.Lime
+        Me.oklb.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.oklb.Location = New System.Drawing.Point(85, 324)
+        Me.oklb.Name = "oklb"
+        Me.oklb.Size = New System.Drawing.Size(250, 100)
+        Me.oklb.TabIndex = 27
+        Me.oklb.Text = "KHÁC KHỔ"
+        Me.oklb.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Button3
+        '
+        Me.Button3.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button3.Location = New System.Drawing.Point(891, 381)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(150, 41)
+        Me.Button3.TabIndex = 25
+        Me.Button3.Text = "Hủy Kết Nối"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 26.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(195, 39)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(792, 39)
+        Me.Label4.TabIndex = 24
+        Me.Label4.Text = "CHƯƠNG TRÌNH ĐIỀU KHIỂN MÁY SONG CẠNH"
+        '
+        'ronglb
+        '
+        Me.ronglb.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.ronglb.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.ronglb.Font = New System.Drawing.Font("Microsoft Sans Serif", 72.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ronglb.Location = New System.Drawing.Point(811, 158)
+        Me.ronglb.Name = "ronglb"
+        Me.ronglb.Size = New System.Drawing.Size(300, 130)
+        Me.ronglb.TabIndex = 23
+        Me.ronglb.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'dailb
+        '
+        Me.dailb.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.dailb.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.dailb.Font = New System.Drawing.Font("Microsoft Sans Serif", 72.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.dailb.Location = New System.Drawing.Point(427, 158)
+        Me.dailb.Name = "dailb"
+        Me.dailb.Size = New System.Drawing.Size(300, 130)
+        Me.dailb.TabIndex = 22
+        Me.dailb.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'daylb
+        '
+        Me.daylb.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.daylb.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.daylb.Font = New System.Drawing.Font("Microsoft Sans Serif", 72.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.daylb.Location = New System.Drawing.Point(87, 158)
+        Me.daylb.Name = "daylb"
+        Me.daylb.Size = New System.Drawing.Size(250, 130)
+        Me.daylb.TabIndex = 21
+        Me.daylb.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(872, 118)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(185, 37)
+        Me.Label3.TabIndex = 20
+        Me.Label3.Text = "Chiều Rộng"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(500, 119)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(157, 37)
+        Me.Label2.TabIndex = 19
+        Me.Label2.Text = "Chiều Dài"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(166, 117)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(123, 37)
+        Me.Label1.TabIndex = 18
+        Me.Label1.Text = "Độ Dày"
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(907, 324)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(114, 41)
+        Me.Button2.TabIndex = 17
+        Me.Button2.Text = "Kết Nối"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'SerialPort1
+        '
+        '
+        'same
+        '
+        Me.same.BackColor = System.Drawing.Color.DeepSkyBlue
+        Me.same.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.same.Location = New System.Drawing.Point(428, 324)
+        Me.same.Name = "same"
+        Me.same.Size = New System.Drawing.Size(300, 100)
+        Me.same.TabIndex = 28
+        Me.same.Text = "CÙNG KHỔ"
+        Me.same.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1169, 455)
+        Me.Controls.Add(Me.same)
+        Me.Controls.Add(Me.oklb)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.ronglb)
+        Me.Controls.Add(Me.dailb)
+        Me.Controls.Add(Me.daylb)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button2)
+        Me.name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents oklb As System.Windows.Forms.Label
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents ronglb As System.Windows.Forms.Label
+    Friend WithEvents dailb As System.Windows.Forms.Label
+    Friend WithEvents daylb As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents SerialPort1 As System.IO.Ports.SerialPort
+    Friend WithEvents same As System.Windows.Forms.Label
+
+End Class
---
+++ WindowsApplication2/Form1.vb
@@ -0,0 +1,185 @@
+﻿
+Imports System
+Imports System.Threading
+Imports System.IO.Ports
+Imports System.ComponentModel
+Imports System.Diagnostics
+
+
+
+Public Class Form1
+    Public watchfolder As New IO.FileSystemWatcher()
+    Public link As String
+    Public com As String
+    Public name As String
+    Public machine As String
+    Public day, dai, rong As String
+    Dim day1, dai1, rong1, send As String
+    Public flag As Boolean
+
+    Delegate Sub SetTextCallback(ByVal [text] As String)
+    'Dim WithEvents ADRport As SerialPort = New System.IO.Ports.SerialPort("COM1", 9600, Parity.Even, 8, StopBits.One)
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        Dim setupReader As String
+        Dim tam As Int16
+        setupReader = My.Computer.FileSystem.ReadAllText("C:\Setup\setup.txt")
+
+        com = Mid(setupReader, 1, 4)
+        link = Mid(setupReader, 7)
+
+
+        For i = (Len(setupReader) - 1) To 0 Step -1
+            If setupReader(i) = "\" Then
+                tam = i
+                Exit For
+            End If
+        Next
+        name = Mid(setupReader, (tam + 1))
+        machine = Mid(name, 2, 2)
+
+
+
+        If My.Computer.FileSystem.FileExists(link) Then
+            My.Computer.FileSystem.DeleteFile(link)
+        End If
+        SerialPort1.PortName = com
+        SerialPort1.BaudRate = 9600
+        SerialPort1.Parity = Parity.None
+        SerialPort1.StopBits = StopBits.One
+        SerialPort1.DataBits = 8
+        If My.Computer.Network.IsAvailable Then
+            Try
+                SerialPort1.Open()
+                Button2.Enabled = False
+                Button3.Enabled = True
+
+                Me.Invoke(Sub()
+                              oklb.Visible = False
+                              same.Visible = False
+                          End Sub)
+
+                watchfolder = New System.IO.FileSystemWatcher()
+                watchfolder.Path = Replace(link, name, "")  'xoa ten file lay folder Replace(setup, "\test.ini", "")  "C:\Setup" '"
+
+
+
+
+                AddHandler watchfolder.Created, AddressOf logchange
+                watchfolder.EnableRaisingEvents = True
+
+            Catch ex As IO.IOException
+                MsgBox("Hãy kiểm tra lại đã đúng cổng COM chưa, nếu chưa vào file C:\Setup\setup.txt để sửa cho đúng, nếu đúng kiểm tra dây từ máy tính đến PLC")
+            End Try
+        Else
+            MsgBox("Không có kết nối mạng, Hãy iểm tra lại kết nối mạng")
+        End If
+        
+    End Sub
+
+    Private Sub Form1_FormClosing(ByVal sender As Object, ByVal e As System.Windows.Forms.FormClosingEventArgs) Handles Me.FormClosing
+        SerialPort1.Close()
+    End Sub
+
+    Private Sub SerialPort1_DataReceived(ByVal sender As Object, ByVal e As System.IO.Ports.SerialDataReceivedEventArgs) Handles SerialPort1.DataReceived
+        Dim a As String
+        a = SerialPort1.ReadExisting
+
+        If a = "1" Then
+            If My.Computer.FileSystem.FileExists(link) Then
+
+                My.Computer.FileSystem.DeleteFile(link)
+                Me.Invoke(Sub()
+                              oklb.Visible = True
+                              same.Visible = False
+                          End Sub)
+            End If
+
+        End If
+
+
+
+
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        SerialPort1.Close()
+        Button2.Enabled = True
+        Button3.Enabled = False
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Button3.Enabled = False
+        Button2.Enabled = True
+    End Sub
+    Private Sub logchange(ByVal source As Object, ByVal e As System.IO.FileSystemEventArgs)
+        Dim fileReader As String
+
+        Dim tam1 As String
+
+
+
+        Try
+            Me.Invoke(Sub()
+                          oklb.Visible = False
+                          same.Visible = False
+                      End Sub)
+           
+            If My.Computer.FileSystem.FileExists(link) Then
+                fileReader = My.Computer.FileSystem.ReadAllText(link)
+                fileReader = fileReader.Replace(fileReader(1), "")
+
+
+                dai = Mid(fileReader, 8, 4)
+                rong = Mid(fileReader, 13, 4)
+                day = Mid(fileReader, 4, 2)
+                If Convert.ToInt16(rong) > Convert.ToInt16(dai) Then
+                    tam1 = rong
+                    rong = dai
+                    dai = tam1
+                End If
+
+                SerialPort1.Write("AA" & day & dai & rong)
+
+                If My.Computer.FileSystem.FileExists(link) Then
+                    My.Computer.FileSystem.DeleteFile(link)
+                End If
+
+                If dai <> dailb.Text Or rong <> ronglb.Text Or day <> daylb.Text Then
+
+                    Me.Invoke(Sub()
+                                  ronglb.Text = rong
+                                  dailb.Text = dai
+                                  daylb.Text = day
+                                  oklb.Visible = True
+                                  same.Visible = False
+                              End Sub)
+                Else
+                    Me.Invoke(Sub()
+                                  same.Visible = True
+                                  oklb.Visible = False
+
+                              End Sub)
+
+                End If
+            End If
+        Catch ex As IO.IOException
+            MsgBox("Có lẽ kết nối máy chủ có vấn đề, phiền bạn kiểm tra kết nối với máy chủ")
+
+        End Try
+    End Sub
+
+   
+
+    Private Sub same_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles same.Click
+
+    End Sub
+
+    Private Sub ronglb_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ronglb.Click
+
+    End Sub
+
+
+
+
+End Class
